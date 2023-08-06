5b0972afb2f2af7774b7e55eac113ef0dd026f93
ilmar521 <118733735+ilmar521@users.noreply.github.com>
2023-08-06 12:24:53 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Admin, Resource } from "react-admin";
+import { dataProvider } from './dataProvider';
+import { UserList } from "./users";
+import { PostList, PostEdit, PostCreate } from "./posts";
+import { Dashboard } from './Dashboard';
+import { authProvider } from './authProvider';
+import PostIcon from "@mui/icons-material/Book";
+import UserIcon from "@mui/icons-material/Group";
+
+export const App = () => (
+  <Admin authProvider={authProvider} dataProvider={dataProvider} dashboard={Dashboard}>   
+    <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon}/>
+    <Resource name="users" list={UserList} recordRepresentation="name" icon={UserIcon}/>
+  </Admin>
+);
